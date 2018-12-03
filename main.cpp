f7399f22ce098233a51e01dd514cfaabfda20bef
DanielFay22 <40331738+DanielFay22@users.noreply.github.com>
2018-12-02 20:53:30 -0600

    Modifies encoder to mimic execution of CUDA kernel

--- main.cpp
+++ main.cpp
@@ -1,81 +1,70 @@
-
 #include <iostream>
-#include <bitset>
 
 using namespace std;
 
-const unsigned long long FILTERS[] = {0b111, 0b011};
-const unsigned long long NUM_FILTERS = 2;
-const unsigned long long FILTER_LEN = 3;
+#define NUM_BITS (8 * sizeof(unsigned int))
+
+const unsigned int data_length = 64;
+
+//const unsigned int THREADS = 256;
+const unsigned int FILTER_LENGTH = 3;
+const unsigned int NUM_FILTERS = 2;
+const unsigned int FILTERS = (0b111 << FILTER_LENGTH) | 0b011;
+
+char nums[2] = { '0', '1' };
+
+#define dataArrayLength ((data_length + NUM_BITS - 1) / NUM_BITS)
+#define resultArrayLength ((data_length * NUM_FILTERS + NUM_BITS - 1) / NUM_BITS)
+
+void printBitString(const unsigned int *data, int length);
 
 
+// This function is specifically structured to mimic the execution of a CUDA kernel,
+// which is why the entire operation is contained in two for loops.
+// The variables "start" and "idx" will be derived from the block index and thread index, respectively.
+void convEncode(const unsigned int *data, unsigned int *output) {
+    for (unsigned int start = 0; start < (data_length + NUM_BITS - 1) / (NUM_BITS * 2); start++)   {
+        for (unsigned int idx = 0; idx < 2 * NUM_BITS; idx++)  {
 
-unsigned long long hammingWeight(unsigned long long num)   {
-    unsigned long long res = 0;
-    while (num) {
-        res += (num & 1);
-        num >>= 1;
-    }
-    return res;
-}
+            unsigned int val = (((idx / NUM_BITS) | start
+                        ? data[idx / NUM_BITS + start - 1] << (1 + (idx % NUM_BITS)) : 0)
+                                | (data[idx / NUM_BITS + start] >> (NUM_BITS - 1 - (idx % NUM_BITS))))
+                                        & ((1 << FILTER_LENGTH) - 1 );
 
-unsigned long long convEncode(unsigned long long data) {
-    unsigned long long res = 0;
+            #pragma unroll
+            for (unsigned int i = 0; i < NUM_FILTERS; i++) {
 
-    for(unsigned int i = 0; i < 32; i ++)    {
+                unsigned int n = val & ((FILTERS >> ((NUM_FILTERS - i - 1) * FILTER_LENGTH))
+                        & ((1 << FILTER_LENGTH) - 1 ));
 
-        unsigned long long mask = (data >> i) & 0b111;
+                n = n - ((n >> 1) & 0x55555555);
+                n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+                unsigned int newVal = (((((n + (n >> 4) & 0x0F0F0F0F) * 0x01010101) >> 24) & 1));
 
-        for (unsigned int j = 0; j < NUM_FILTERS; j ++)   {
-            res |= ((hammingWeight(mask & FILTERS[j]) & 1) << (NUM_FILTERS * (i + 1) - 1 - j));
+                output[NUM_FILTERS * start + (NUM_FILTERS * idx) / NUM_BITS] |=
+                        (newVal << ((NUM_BITS * NUM_FILTERS - 1 - (NUM_FILTERS * (idx % NUM_BITS) + i))));
+            }
         }
     }
-
-    return res;
 }
 
-int main(int argc, char* argv[]) {
-
-    if (argc < 2) {
-        cerr << "Improper Number of Arguments" << endl;
-        return 1;
+void printBitString(const unsigned int *data, int length) {
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < NUM_BITS; j++) {
+            cout << nums[(data[i] >> (NUM_BITS - 1 - j)) & 1];
+        }
     }
-
-    string data_string = argv[1];
-    string res_string;
-
-    unsigned int original_len = data_string.length();
-
-    unsigned int block_size = 8 * sizeof(unsigned long long) / NUM_FILTERS;
-    unsigned int num_blocks = (data_string.size() + block_size - 1) / block_size;
-
-    unsigned long long res;
-
-//    cout << res << endl;
-
-    while (data_string.size() - FILTER_LEN + 2 < block_size * num_blocks)    {
-        data_string.insert(0, "0");
-    }
-
-    for (unsigned int i = 0; i < num_blocks; i ++)    {
-        std::string sub = data_string.substr(i * block_size,
-                (i + 1) * block_size + (unsigned int)NUM_FILTERS - 1);
-
-        res = convEncode(std::bitset<64>(sub).to_ullong());
-
-        res_string += std::bitset<64>(res).to_string();
-//        cout << res[i] << endl;
-//        cout << bitset<64>(res[i]).to_string() << endl;
-    }
-    if (res_string.length() > 2 * original_len) {
-        res_string = res_string.substr(res_string.length() - 2 * original_len);
-    }
-
-    cout << res_string << endl;
-
-    return 0;
+    cout << endl;
 }
 
 
+int main() {
+    unsigned int input[dataArrayLength] = {0xFFFFFFFF, 0xFFFFFFFF};
+    unsigned int output[resultArrayLength];
 
+    for (unsigned int &i : output) { i = 0; }
 
+    convEncode(input, output);
+    printf("%x\n%x\n", output[0], output[1]);
+    printBitString(output, resultArrayLength);
+}
